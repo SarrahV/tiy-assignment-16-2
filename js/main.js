e471085618d46e66c441066b33b0f06dc1db188d
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-25 07:47:06 -0500

    worked out a few tests and functions

--- js/main.js
+++ js/main.js
@@ -14,22 +14,36 @@ window.Exhausted = (function() {
 
     //----
 
+    sayGoodnight: function(name) {
+      return "Goodnight " + name;
+    },
+
+
+    //----
+    
+    reverseMe: function(str) {
+      return str.split("").reverse().join("");
+    },
 
 
     //----
 
+   add5: function(num) {
+      return num + 5;
+    },
+
+    //----
+    
+    add50: function(num){
+      return num += 50;
+    },
 
 
     //----
 
-   
-
-    //----
-
-
-
-    //----
-
+    multiplyby10: function(num){
+      return num *= 10;
+    },
 
 
     //----
--- test/test.main.js
+++ test/test.main.js
@@ -8,7 +8,49 @@ describe("Exhausted", function() { //start
   describe("squared", function() {
 
     it("multiplies a number by itself", function() {
-      expect( exhausted.squared(4) ).eq(16);
+      expect( exhausted.squared(2) ).eq(4);
+    });
+  });
+
+  describe("sayGoodnight", function() {
+
+    it("says goodnight to a name", function() {
+      expect( exhausted.sayGoodnight("Sarrah") ).eq("Goodnight Sarrah");
+    });
+  });
+
+  describe("reverseMe", function() {
+
+    it("reverses a string", function() {
+      expect( exhausted.reverseMe("Sarrah") ).eq("harraS");
+    });
+  });
+  
+  describe("add5", function() {
+
+    it("adds 5 to a number", function() {
+      expect( exhausted.add5(5) ).eq(10);
+    });
+  });
+
+  describe("add50", function() {
+
+    it("is a method", function() {
+      expect( typeof exhausted.add50 ).eq("function");
+    });
+
+    it("adds 50 to a number", function() {
+      expect( exhausted.add50(25) ).eq(75);
+    });
+  });
+  describe("multBy10", function() {
+
+    it("is a method", function() {
+      expect( typeof exhausted.multBy10 ).eq("function");
+    });
+
+    it("multiplies a number by 10", function() {
+      expect( exhausted.multBy10(50) ).eq(500);
     });
   });
 
